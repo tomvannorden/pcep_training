7900fde06cac815d9e4b356000478522c9c88737
Tom van Norden <tomvannorden1@gmail.com>
2022-03-09 13:14:01 +0000

    Updated the script

--- to-celsius.py
+++ to-celsius.py
@@ -1,3 +1,5 @@
-fahrenheit = float(input("What value of fahrenheit would you like to convert: "))
+#!/usr/bin/env python3.7
+
+fahrenheit = float(input("What value of fahrenheit would you like to convert to celsius?: "))
 celsius = round((fahrenheit-32) * (5/9), 1)
-print("The converted temperature is " + str(celsius) + " celsius")
+print(str(fahrenheit) + "F is", str(celsius) + "C")
