ca54ef3f357fe421f2badedd5037165a3c9f2d18
Tom van Norden <tomvannorden1@gmail.com>
2022-03-10 14:36:28 +0000

    Added list practice script

---
+++ users_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+
+users_list = ['tom', 'bob', 'mark']
+
+index_bob = users_list.index('bob')
+print(index_bob)
+
+users_list.append('melody')
+print(users_list)
+
+print(users_list[2:4])
